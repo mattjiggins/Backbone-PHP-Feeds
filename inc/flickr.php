f595448781f021f9462f9228ff212a61e341903b
matt jiggins <git@jiggins.ca>
2015-04-17 15:02:34 +0200

    Access Control To All

--- inc/flickr.php
+++ inc/flickr.php
@@ -31,6 +31,7 @@ if ( !$modified || ( ( $now - $modified ) > $interval ) ) {
   }
 }
 
+header("Access-Control-Allow-Origin: *");
 header( 'Cache-Control: no-cache, must-revalidate' );
 header( 'Expires: Mon, 26 Jul 1997 05:00:00 GMT' );
 header( 'Content-type: application/json' );
